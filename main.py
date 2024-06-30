18c9fbd7223bc13b71b4c2bffdfc3e6f6fc5c114
happyness980817 <happyness980817@gmail.com>
2024-06-30 16:18:32 +0900

    send messages (project completed)

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def home():
         # kind of a temporary data stored on a server
         # don't want to ask the users for a name and a room code every single time they refresh the web page.
         return redirect(url_for("room"))
+    
     return render_template("home.html")
 
 @app.route("/room")
@@ -54,7 +55,52 @@ def room():
     if room is None or session.get("name") is None or room not in rooms: 
         return redirect(url_for("home"))
 
-    return render_template("room.html")
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name":name, "message":"has entered the room"}, to=room) # sends messages to everyone in the room
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def diconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room] # delete room when every user leaves
+
+    send({"name":name, "message":"has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True) # for development, automatically refreshes
